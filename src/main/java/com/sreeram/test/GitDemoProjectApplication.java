e4855b5cd39a3f7490a5802825d626d671faeef9
RAM <RAM@DESKTOP-J8GLC0J>
2020-03-19 11:35:58 +0530

    First commit

---
+++ src/main/java/com/sreeram/test/GitDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sreeram.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sreeram/test/GitDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreeram.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
